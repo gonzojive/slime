03b88b0bcf91141073a3e16eedd2bbba45e3e2b0
Red Daly <reddaly@gmail.com>
2010-07-17 03:11:34 -0700

    Added :proxy to swank events that get forwarded along to slime

--- swank.lisp
+++ swank.lisp
@@ -1231,7 +1231,8 @@ The processing is done in the extent of the toplevel restart."
        :presentation-start :presentation-end
        :new-package :new-features :ed :indentation-update
        :eval :eval-no-wait :background-message :inspect :ping
-       :y-or-n-p :read-from-minibuffer :read-string :read-aborted)
+       :y-or-n-p :read-from-minibuffer :read-string :read-aborted
+       :proxy-event)
       &rest _)
      (declare (ignore _))
      (encode-message event (current-socket-io)))
